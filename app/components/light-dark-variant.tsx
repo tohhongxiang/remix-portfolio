0afa7af166cb6b17ff00cfb5a6595baf7129d2b7
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-02-22 02:32:49 +0800

    Add animations to hero section

--- app/components/light-dark-variant.tsx
+++ app/components/light-dark-variant.tsx
@@ -1,4 +1,10 @@
-interface LightDarkVariantProps {
+import { cn } from "~/lib/utils";
+
+interface LightDarkVariantProps
+    extends React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLDivElement>,
+        HTMLDivElement
+    > {
     light: React.ReactNode;
     dark: React.ReactNode;
 }
@@ -6,13 +12,14 @@ interface LightDarkVariantProps {
 export default function LightDarkVariant({
     light,
     dark,
+    ...props
 }: LightDarkVariantProps) {
     return (
-        <div className="inline-flex items-center justify-center">
+        <div {...props} className={cn("relative", props.className)}>
             <div className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0">
                 {light}
             </div>
-            <div className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100">
+            <div className="absolute top-1/2 -translate-y-1/2 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100">
                 {dark}
             </div>
         </div>
