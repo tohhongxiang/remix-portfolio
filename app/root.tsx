9a9be0b78b2f9649203c62b36d4947c93f995b88
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-02-26 01:06:45 +0800

    Add gstatic preconnect and move loading of font to link

--- app/root.tsx
+++ app/root.tsx
@@ -34,6 +34,15 @@ export const links: LinksFunction = () => [
         href: "https://fonts.googleapis.com",
         crossOrigin: "anonymous",
     },
+    {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+    },
+    {
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap",
+        rel: "stylesheet",
+    },
     ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
