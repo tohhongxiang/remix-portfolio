04602e752eb53061d90f35ba4f8af270a155a894
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-02-28 01:46:59 +0800

    Change light-dark-variant to use framer-motion for animation to remove need to load both elements

--- app/components/light-dark-variant.tsx
+++ app/components/light-dark-variant.tsx
@@ -1,4 +1,6 @@
 import { cn } from "~/lib/utils";
+import { AnimatePresence, LazyMotion, domAnimation, m } from "framer-motion";
+import { Theme, useTheme } from "remix-themes";
 
 interface LightDarkVariantProps
     extends React.DetailedHTMLProps<
@@ -9,19 +11,57 @@ interface LightDarkVariantProps
     dark: React.ReactNode;
 }
 
+const animationVariants = {
+    initial: { rotate: 90, scale: 0 },
+    animate: { rotate: 0, scale: 1 },
+    exit: { rotate: -90, scale: 0 },
+};
+
 export default function LightDarkVariant({
     light,
     dark,
     ...props
 }: LightDarkVariantProps) {
+    const [theme] = useTheme();
+    console.log(theme);
+
     return (
         <div {...props} className={cn("relative", props.className)}>
-            <div className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0">
-                {light}
-            </div>
-            <div className="absolute top-1/2 -translate-y-1/2 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100">
-                {dark}
-            </div>
+            <LazyMotion features={domAnimation}>
+                <AnimatePresence mode="popLayout" initial={false}>
+                    {theme === Theme.LIGHT ? (
+                        <m.div
+                            variants={animationVariants}
+                            initial={"initial"}
+                            animate={"animate"}
+                            exit={"exit"}
+                            transition={{
+                                type: "spring",
+                                bounce: 0,
+                                duration: 0.2,
+                            }}
+                            key="light"
+                        >
+                            {light}
+                        </m.div>
+                    ) : (
+                        <m.div
+                            key="dark"
+                            variants={animationVariants}
+                            initial={"initial"}
+                            animate={"animate"}
+                            exit={"exit"}
+                            transition={{
+                                type: "spring",
+                                bounce: 0,
+                                duration: 0.2,
+                            }}
+                        >
+                            {dark}
+                        </m.div>
+                    )}
+                </AnimatePresence>
+            </LazyMotion>
         </div>
     );
 }
